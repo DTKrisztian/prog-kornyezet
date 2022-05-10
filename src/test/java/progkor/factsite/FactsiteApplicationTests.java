fb25ce1291e527d0dc4f96c96024372dd2203e4c
DTKrisztian <diofasi.krisztian@gmail.com>
2022-05-10 18:26:53 +0200

    Added spring and maven basic files Added pom.xml

---
+++ src/test/java/progkor/factsite/FactsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package progkor.factsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
